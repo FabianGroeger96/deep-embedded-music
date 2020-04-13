f6bd46d135b51001958cff7749770c70a7ef5fa0
FabianGroeger96 <fabian.groeger@bluewin.ch>
2020-04-13 09:32:46 +0200

    added evaluation to classifier

--- src/train_classifier.py
+++ src/train_classifier.py
@@ -5,6 +5,7 @@ import os
 import tensorflow as tf
 
 from src.feature_extractor.extractor_factory import ExtractorFactory
+from src.input_pipeline.base_dataset import DatasetType
 from src.input_pipeline.dataset_factory import DatasetFactory
 from src.input_pipeline.triplet_input_pipeline import TripletsInputPipeline
 from src.models.classifier import Classifier
@@ -19,6 +20,88 @@ parser.add_argument("--dataset_dir", default="DCASE",
                     help="Dataset directory containing the model")
 parser.add_argument("--model_to_load", default="results/ConvNet1D-20200326-065709",
                     help="Model to load")
+
+
+def train():
+    dataset_iterator = pipeline.get_dataset(extractor, dataset_type=DatasetType.TRAIN, shuffle=params.shuffle_dataset)
+    # iterate over the batches of the dataset
+    for batch_index, (anchor, neighbour, opposite, triplet_labels) in enumerate(dataset_iterator):
+        emb_anchor = model(anchor, training=False)
+        emb_neighbour = model(neighbour, training=False)
+        emb_opposite = model(opposite, training=False)
+
+        # record the operations run during the forward pass, which enables auto differentiation
+        with tf.GradientTape() as tape:
+            # run the forward pass of the layer
+            pred_anchor = classifier(emb_anchor)
+            pred_neighbour = classifier(emb_neighbour)
+            pred_opposite = classifier(emb_opposite)
+
+            pred = tf.concat([pred_anchor, pred_neighbour, pred_opposite], axis=0)
+            labels = tf.concat([triplet_labels[:, 0], triplet_labels[:, 1], triplet_labels[:, 2]], axis=0)
+
+            c_loss = classifier_loss_fn(y_true=labels, y_pred=pred)
+
+        # GradientTape automatically retrieves the gradients of the trainable variables with respect to the loss
+        grads = tape.gradient(c_loss, classifier.trainable_weights)
+        # update the values of the trainable variables to minimize the loss
+        classifier_optimizer.apply_gradients(zip(grads, classifier.trainable_weights))
+
+        classifier_loss(c_loss)
+        classifier_loss_epochs(c_loss)
+
+        classifier_accuracy(labels, pred)
+        classifier_accuracy_epochs(labels, pred)
+
+        # write batch losses to summary writer
+        with train_summary_writer.as_default():
+            # write summary of batch losses
+            tf.summary.scalar("classifier/train_loss_batches", classifier_loss.result(),
+                              step=int(ckpt_classifier.step))
+            tf.summary.scalar("classifier/train_accuracy_batches", classifier_accuracy.result(),
+                              step=int(ckpt_classifier.step))
+
+        logger.info("Classifier loss: {0}".format(c_loss))
+
+        # add one step to checkpoint
+        ckpt_classifier.step.assign_add(1)
+
+    # write epoch loss to summary writer
+    with train_summary_writer.as_default():
+        # write summary of epoch loss
+        tf.summary.scalar("classifier/train_loss_epochs", classifier_loss_epochs.result(), step=epoch)
+        tf.summary.scalar("classifier/train_accuracy_epochs", classifier_accuracy_epochs.result(), step=epoch)
+
+
+def evaluate():
+    dataset_iterator = pipeline.get_dataset(extractor, dataset_type=DatasetType.EVAL, shuffle=params.shuffle_dataset)
+    # iterate over the batches of the dataset
+    for batch_index, (anchor, neighbour, opposite, triplet_labels) in enumerate(dataset_iterator):
+        emb_anchor = model(anchor, training=False)
+        emb_neighbour = model(neighbour, training=False)
+        emb_opposite = model(opposite, training=False)
+
+        # record the operations run during the forward pass, which enables auto differentiation
+        with tf.GradientTape() as tape:
+            # run the forward pass of the layer
+            pred_anchor = classifier(emb_anchor)
+            pred_neighbour = classifier(emb_neighbour)
+            pred_opposite = classifier(emb_opposite)
+
+            pred = tf.concat([pred_anchor, pred_neighbour, pred_opposite], axis=0)
+            labels = tf.concat([triplet_labels[:, 0], triplet_labels[:, 1], triplet_labels[:, 2]], axis=0)
+
+            c_loss = classifier_loss_fn(y_true=labels, y_pred=pred)
+
+        classifier_eval_loss_epochs(c_loss)
+        classifier_eval_accuracy_epochs(labels, pred)
+
+    # write epoch loss to summary writer
+    with train_summary_writer.as_default():
+        # write summary of epoch loss
+        tf.summary.scalar("classifier/eval_loss_epochs", classifier_eval_loss_epochs.result(), step=epoch)
+        tf.summary.scalar("classifier/eval_accuracy_epochs", classifier_eval_accuracy_epochs.result(), step=epoch)
+
 
 if __name__ == "__main__":
     # load the arguments
@@ -68,11 +151,14 @@ if __name__ == "__main__":
     # define triplet loss metrics
     classifier_accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
     classifier_accuracy_epochs = tf.keras.metrics.SparseCategoricalAccuracy()
+    classifier_eval_accuracy_epochs = tf.keras.metrics.SparseCategoricalAccuracy()
+
     classifier_loss = tf.keras.metrics.Mean("classifier_accuracy", dtype=tf.float32)
     classifier_loss_epochs = tf.keras.metrics.Mean("classifier_accuracy_epochs", dtype=tf.float32)
+    classifier_eval_loss_epochs = tf.keras.metrics.Mean("classifier_accuracy_epochs", dtype=tf.float32)
 
     # define checkpoint and checkpoint manager
-    ckpt = tf.train.Checkpoint(step=tf.Variable(1), optimizer=optimizer, net=model)
+    ckpt = tf.train.Checkpoint(net=model)
     manager = tf.train.CheckpointManager(ckpt, saved_model_path, max_to_keep=3)
 
     # check if models has been trained before
@@ -82,64 +168,19 @@ if __name__ == "__main__":
     else:
         logger.info("Initializing models from scratch.")
 
+    ckpt_classifier = tf.train.Checkpoint(step=tf.Variable(1), net=classifier)
+
     for epoch in range(params.epochs):
         logger.info("Starting epoch {0} from {1}".format(epoch + 1, params.epochs))
-        dataset_iterator = pipeline.get_dataset(extractor, shuffle=params.shuffle_dataset)
-        dataset_iterator = iter(dataset_iterator)
-        # iterate over the batches of the dataset
-        for batch_index, (anchor, neighbour, opposite, triplet_labels) in enumerate(dataset_iterator):
-            emb_anchor = model(anchor, training=False)
-            emb_neighbour = model(neighbour, training=False)
-            emb_opposite = model(opposite, training=False)
 
-            # record the operations run during the forward pass, which enables auto differentiation
-            with tf.GradientTape() as tape:
-                # run the forward pass of the layer
-                pred_anchor = classifier(emb_anchor)
-                pred_neighbour = classifier(emb_neighbour)
-                pred_opposite = classifier(emb_opposite)
-
-                pred = tf.concat([pred_anchor, pred_neighbour, pred_opposite], axis=0)
-                labels = tf.concat([triplet_labels[:, 0], triplet_labels[:, 1], triplet_labels[:, 2]], axis=0)
-
-                c_loss = classifier_loss_fn(y_true=labels, y_pred=pred)
-
-            # GradientTape automatically retrieves the gradients of the trainable variables with respect to the loss
-            grads = tape.gradient(c_loss, classifier.trainable_weights)
-            # update the values of the trainable variables to minimize the loss
-            classifier_optimizer.apply_gradients(zip(grads, classifier.trainable_weights))
-
-            # add losses to the metrics
-            classifier_loss(c_loss)
-            classifier_loss_epochs(c_loss)
-
-            classifier_accuracy(labels, pred)
-            classifier_accuracy_epochs(labels, pred)
-
-            # write batch losses to summary writer
-            with train_summary_writer.as_default():
-                # write summary of batch losses
-                tf.summary.scalar("classifier/loss_batches", classifier_loss.result(), step=int(ckpt.step))
-                tf.summary.scalar("classifier/accuracy_batches", classifier_accuracy.result(), step=int(ckpt.step))
-
-            logger.info("Classifier loss: {0}".format(c_loss))
-
-            # add one step to checkpoint
-            # todo - new ckpt for classifier
-            ckpt.step.assign_add(1)
-
-        # save the current model after a specified amount of epochs
-        # TODO - saving classifier
-        if epoch % params.save_frequency == 0 and bool(params.save_model):
-            manager_save_path = manager.save()
-            logger.info("Saved checkpoint for epoch {0}: {1}".format(epoch, manager_save_path))
-
-        # write epoch loss to summary writer
-        with train_summary_writer.as_default():
-            # write summary of epoch loss
-            tf.summary.scalar("classifier/loss_epochs", classifier_loss_epochs.result(), step=epoch)
-            tf.summary.scalar("classifier/accuracy_epochs", classifier_accuracy_epochs.result(), step=int(ckpt.step))
+        train()
+        evaluate()
 
         # reset metrics every epoch
+        classifier_accuracy.reset_states()
+        classifier_accuracy_epochs.reset_states()
+        classifier_eval_accuracy_epochs.reset_states()
+
         classifier_loss.reset_states()
         classifier_loss_epochs.reset_states()
+        classifier_eval_loss_epochs.reset_states()
