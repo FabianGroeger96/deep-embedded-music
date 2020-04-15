52a0481432fcf513ecd79340348c69e4c0400023
FabianGroeger96 <fabian.groeger@bluewin.ch>
2020-04-15 15:58:07 +0200

    added memory growth

--- src/train_triplet_loss.py
+++ src/train_triplet_loss.py
@@ -41,6 +41,18 @@ def log_step(logger, epoch, batch_index, batch_size, result, log_level: LogLevel
 
 
 def main():
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
+
     # load the parameters from json file
     args = parser.parse_args()
     json_path = os.path.join(args.experiment_dir, "config", "params.json")
