2743d209230e08bccac4b5603afa48729c40afe4
Fabian Gröger <fabian.groeger@bluewin.ch>
2020-05-06 08:20:27 +0200

    refactoring

---
+++ oninstall.sh
@@ -0,0 +1,11 @@
+apt-get update
+
+apt-get -y install git
+apt-get -y install libav-tools
+apt-get -y install libsndfile1-dev
+apt-get -y install libsndfile1
+
+pip install --upgrade pip
+pip install -r requirements.txt
+
+pip install -q -U -q tensorboard_plugin_profile
--- onstart.sh
+++ onstart.sh
@@ -1,13 +1,3 @@
-apt-get update
-
-apt-get -y install git
-apt-get -y install libav-tools
-apt-get -y install libsndfile1-dev
-apt-get -y install libsndfile1
-
-pip install --upgrade pip
-pip install -r requirements.txt
-
-pip install -q -U -q tensorboard_plugin_profile
+./oninstall.sh
 
 python -m src.train_triplet_loss
